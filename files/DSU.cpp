16ce2ab7175ea2e9eb7dd90db6a0ceefc65689b6
Abhishek Kainth <akainth@expedia.com>
2019-10-01 01:34:37 +0530

    Adding DSU template

---
+++ files/DSU.cpp
@@ -0,0 +1,35 @@
+//---------------------------------------------------
+// Disjoint Set Union
+
+#define MAXN 1005
+
+int parent[MAXN],rank[MAXN];
+
+void create_set(int n){
+	
+	memset(rank,0,sizeof(int)*(n+1));	
+	for(int i=1; i <= n; i++)
+		// initially all nodes are disjoint (hence no parent)
+		parent[i] = i;
+}
+int find_parent(int x){
+	
+	if(parent[x] == x)
+		return x;	
+	parent[x] = find_parent(parent[x]); // path-compression	
+	return parent[x];
+}
+void merge(int x,int y){
+	
+	int px = find_parent(x), py = find_parent(y);
+	
+	if(rank[px] > rank[py]) 
+		parent[py] = px;
+	else 
+		parent[px] = py;
+	
+	if(rank[py] == rank[px]) 
+		rank[py]++;
+}
+
+//-----------------------------------------------------
